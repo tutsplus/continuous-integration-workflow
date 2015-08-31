9a70d7f5c428ea17602bce4c020420d26f8bdd25
Markus Mühlberger <markus@mmuehlberger.com>
2015-08-31 15:18:08 +0200

    Author adding and editing

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,48 @@
 class AuthorsController < ApplicationController
+  before_action :find_author, only: [:edit, :update, :destroy]
+
   def index
     @authors = Author.order(last_name: :asc, first_name: :asc)
   end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+
+    if @author.save
+      redirect_to authors_path, success: 'The author has been created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @author.update(author_params)
+      redirect_to authors_path, success: 'The author has been updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @author.destroy
+    redirect_to authors_path, success: 'The author has been deleted.'
+  end
+
+  private
+
+  def find_author
+    @author = Author.find(params[:id])
+  end
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name)
+  end
+
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'requires a first name' do
+    author = FactoryGirl.build(:author, first_name: '')
+    expect(author).to_not be_valid
+  end
+
+  it 'requires a last name' do
+    author = FactoryGirl.build(:author, last_name: '')
+    expect(author).to_not be_valid
+  end
 end

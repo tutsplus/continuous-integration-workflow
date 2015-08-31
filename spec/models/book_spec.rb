07aaddbb3d05f13816ccaf5e141cd5fb9015087a
Markus Mühlberger <markus@mmuehlberger.com>
2015-08-31 20:26:36 +0200

    Add tests for publisher model validation

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+  it 'requires a name' do
+    publisher = FactoryGirl.build(:publisher, name: '')
+    expect(publisher).to_not be_valid
+  end
 end
